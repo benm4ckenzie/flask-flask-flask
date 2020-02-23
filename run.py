ed2e691f5092b6dcd532a6197f0932be6630269c
benm4ckenzie <b_m@me.com>
2020-02-23 21:36:22 +0000

    updated interactivity for contact and about

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
 
 @app.route('/')
@@ -17,9 +18,22 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
 
-@app.route('/contact')
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+
+    return render_template("member.html", member=member)
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method=="POST":
+        flash("Thanks {}, we have received your message.".format(request.form["name"]))
     return render_template("contact.html", page_title="Contact")
 
 
@@ -29,5 +43,5 @@ def careers():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+            port=(os.environ.get('PORT')),
             debug=True)
